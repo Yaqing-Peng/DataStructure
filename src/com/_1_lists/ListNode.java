32b34dd11bae7e07b7fe669c0ce8db077b5626be
Jasmine7595 <759560274@qq.com>
2022-10-07 12:45:38 +0800

    fix

--- src/com/_1_lists/ListNode.java
+++ src/com/_1_lists/ListNode.java
@@ -7,4 +7,8 @@ public class ListNode {
     public ListNode(int val) {
         value = val;
     }
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
